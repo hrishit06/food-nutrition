66d139cf978babf4a60747469ae01aa8964d0432
hrishit06 <hrishitsaha@gmail.com>
2025-12-07 21:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
@@ -125,5 +124,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
